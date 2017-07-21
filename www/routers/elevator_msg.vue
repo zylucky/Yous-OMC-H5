cb6723f3ddaf7f382f3023bd2faa9bedce895e52
qizhenzhen <qizhenzhen2017@126.com>
2017-07-21 09:58:17 +0800

    电梯信息

--- www/routers/elevator_msg.vue
+++ www/routers/elevator_msg.vue
@@ -1,6 +1,7 @@
 <style scoped lang="less">
   @import "../resources/css/reset.css";
   @import "../resources/css/base.less";
+  @import "../resources/css/website/elevator_msg.less";
 
   .page-infinite-loading {
     text-align: center;
@@ -13,129 +14,64 @@
 </style>
 <template>
   <div class="all_elements">
-    <div class="build_top">
+    <div class="build_top ele_wrap">
       <ul class="ys_item_ul mb60">
+        <li class="clearfix">
+          <span class="ys_tit w224">电梯品牌：</span>
+          <div class="ys_item_con fl">
+            <input type="text" value="" placeholder="请输入">
+          </div>
+        </li>
+        <li class="clearfix">
+          <span class="ys_tit w224">客梯数量：</span>
+          <div class="ys_item_con fl">
+            <input type="text" value="" placeholder="请输入">
+          </div>
+        </li>
+        <li class="clearfix">
+          <span class="ys_tit w224">客梯层级区分：</span>
+          <div class="ys_item_con fl">
+            <input type="radio" name="ele_type" id="ele_yes">
+            <label for="ele_yes" class="mr20">是</label>
+            <input type="radio" name="ele_type">
+            <label for="ele_no" id="ele_no">否</label>
+          </div>
+        </li>
+        <li class="clearfix mb20">
+          <span class="ys_tit w224">所到楼层：</span>
+          <div class="ys_item_con fl">
+            <input type="text" class="inp_sm" value="" placeholder="请输入">
+            <span class="hor_line"></span>
+            <input type="text" class="inp_sm" value="" placeholder="请输入">
+          </div>
+        </li>
 
         <li class="clearfix">
-          <span class="ys_tit">开发商：</span>
+          <span class="ys_tit w224">货梯数量：</span>
           <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请输入">
+            <input type="text inp_sm" value="" placeholder="请输入">
           </div>
         </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">开盘日期：</span>
-          <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请选择日期">
-            <i class="calendar_icon"></i>
-          </div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">楼盘级别：</span>
-          <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请选择">
-            <i class="right_arrow">&gt;</i>
-          </div>
-        </li>
+
         <li class="clearfix">
-          <span class="ys_tit">产权性质：</span>
-          <div class="ys_item_con fl"><a href="javascript:;" class="cl_link">请选择标签</a></div>
-        </li>
-        <li class="clearfix">
-          <span class="ys_tit">特色标签：</span>
-          <div class="ys_item_con fl w570">
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">写字楼</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">公寓</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">商务楼</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">住宅</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">商业</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">酒店</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">别墅</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">综合</label>
-            </div>
-            <div class="check_wrap clearfix">
-              <input type="checkbox" value="" name="">
-              <label class="fl">商业综合体</label>
-            </div>
-          </div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">楼盘品质：</span>
+          <span class="ys_tit w224">货梯层级区分：</span>
           <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请选择">
-            <i class="right_arrow">&gt;</i>
+            <input type="radio" name="ele_type" id="frei_ele_yes">
+            <label for="ele_yes" class="mr20">是</label>
+            <input type="radio" name="ele_type">
+            <label for="ele_no" id="frei_ele_no">否</label>
           </div>
         </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">楼盘均价：</span>
+
+        <li class="clearfix mb20">
+          <span class="ys_tit w224">所到楼层：</span>
           <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请输入">
-            <i class="right_unit">元/m³/天</i>
+            <input type="text" class="inp_sm" value="" placeholder="请输入">
+            <span class="hor_line"></span>
+            <input type="text" class="inp_sm" value="" placeholder="请输入">
           </div>
         </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">使用率：</span>
-          <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请选择">
-            <i class="right_arrow">&gt;</i>
-          </div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">空置比例：</span>
-          <div class="cl_999">自动生成</div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit pt10">
-            <i class="ys_tit_sm">装修配套设施明细</i>
-          </span>
-          <div class="ys_item_con fl pt10">
-            <input type="text" value="" placeholder="请输入">
-          </div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit">空置比例：</span>
-          <div class="cl_999">自动生成</div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit w224">楼盘设计公司：</span>
-          <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请输入">
-          </div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit w224">楼盘设计师：</span>
-          <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请输入">
-          </div>
-        </li>
-        <li class="clearfix pr">
-          <span class="ys_tit w224">楼盘设计师风格：</span>
-          <div class="ys_item_con fl">
-            <input type="text" value="" placeholder="请输入">
-          </div>
-        </li>
+
       </ul>
       <a href="javascript:;" class="ys_default_btn mb80">保存</a>
     </div>
