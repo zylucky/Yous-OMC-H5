236dea5fd2a429015d01180524400b8edfae5c2f
qizhenzhen <qizhenzhen2017@126.com>
2017-07-28 17:04:19 +0800

    login6

--- www/routers/login.vue
+++ www/routers/login.vue
@@ -10,6 +10,10 @@
     }
   }
 
+  .mt5rem {
+    margin-top: .5rem;
+  }
+
 </style>
 <template>
   <div class="all_elements" id="ys_user">
@@ -19,12 +23,15 @@
           <div class="inp_item">
             <div class="inp_bg"></div>
             <i class="ys_icon user"></i>
-            <input type="text" style="margin-top: .5rem;" value="" v-model="username" placeholder="请输入用户名">
+            <input type="text" id="userName"
+                   @focus="focus"
+                   class="mt5rem" value=""
+                   v-model="username" placeholder="请输入用户名">
           </div>
           <div class="inp_item">
             <div class="inp_bg"></div>
             <i class="ys_icon pass"></i>
-            <input type="text" value="" style="margin-top: .5rem;" v-model="password" placeholder="请输入密码">
+            <input type="text" value="" class="mt5rem" v-model="password" placeholder="请输入密码">
           </div>
           <div class="inp_item login">
             <a href="javascript:;" style="line-height: 1.4rem;margin-top: .14rem" class="ys_btn" @click="login">登 录</a>
@@ -35,9 +42,9 @@
   </div>
 </template>
 <script>
-  import { Toast } from 'mint-ui'; //toast
+  import {Toast} from 'mint-ui'; //toast
 
-  import { MessageBox } from 'mint-ui'; //弹窗
+  import {MessageBox} from 'mint-ui'; //弹窗
 
   export default {
     components: {},
@@ -50,25 +57,34 @@
     },
     methods: {
       login(){
-          var _this=this;
+        var _this = this;
         if (this.username == 'admin' && this.password == 'admin') {
           Toast({
             message: '恭喜您！登陆成功',
             position: 'bottom',
             duration: 3000
           });
-          setTimeout(function(){
-            _this.$router.push({path:'/list'});
-          },1500);
+          setTimeout(function () {
+            _this.$router.push({path: '/list'});
+          }, 1500);
 
-        }else if(this.username == '' || this.password == ''){
+        } else if (this.username == '' || this.password == '') {
           MessageBox('提示', '请输入用户名和密码');
-        }else{
+        } else {
           MessageBox('提示', '用户名或密码错误，请重新输入！');
-        }}
+        }
       },
-      mounted(){
 
-      },
-    }
+      focus(){
+        var _this = this;
+        setTimeout(function () {
+          let pannel = _this.$refs.userName;
+          pannel.scrollIntoView(true);
+        }, 200);
+      }
+    },
+    mounted(){
+
+    },
+  }
 </script>
