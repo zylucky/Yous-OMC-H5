e6d08bbdee271321bd23b2dc60775ec55290101c
yuan <zylucying@163.com>
2018-01-08 08:42:59 +0800

    销售bug提交2018-01-08

--- www/pages/channel/news.vue
+++ www/pages/channel/news.vue
@@ -143,12 +143,13 @@ export default{
             });
 		},
 		takenews(){//接收消息
-			const url = "http://erp.youshikongjian.com/receiveMessage/"+ this.userid + "/sys/qd";//消息接口地
+			const url = "http://www.youshikongjian.com/receiveMessage/"+ this.userid + "/sys/qd";//消息接口地
 			axios.get(url, {
 				
 			}).then((res)=>{
-				console.log('=============')
-				console.log(res)
+				if(res.data.success){
+					console.log(res)					
+				}
             }, (err)=>{
 				console.log(err);
             });
