76a114c6c31a3116ccdbb2acd35e7475fb7ba04f
wangqian188 <m18811383660_1@163.com>
2018-01-19 09:21:04 +0800

    Update main.js

--- www/main.js
+++ www/main.js
@@ -13,8 +13,8 @@ Vue.use(VueResource);
 Vue.use(MintUI);
 
 
-Vue.prototype.$prefix = "http://47.92.145.21:81" //图片前缀
-//Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
+Vue.prototype.$prefix = "http://47.92.145.21:81" //图片前缀11111122222
+//Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀111111
 
 // 测试环境
 //Vue.prototype.$api = "http://192.168.0.222:8080" //api地址
@@ -229,7 +229,7 @@ var router = new VueRouter({
 });
 
 
-
+//线上
 router.beforeEach(function(to, from, next){
     const user = JSON.parse(localStorage.getItem('loginxs'));
     if (!user && to.path != '/login') {
@@ -254,7 +254,8 @@ router.beforeEach(function(to, from, next){
                 }else{
                     next({path: '/login'});
                 }
-                $.post("http://omc.urskongjian.com/yhcms/web/wxqx/getXsLogin.do", {
+                alert(user22.sjs);
+                $.post("http://116.62.68.26:8080/yhcms/web/wxqx/getXsLogin.do", {
                         "foreEndType": 2,
                         "code": "300000045",
                         "cookie": user22.sjs,
@@ -264,6 +265,7 @@ router.beforeEach(function(to, from, next){
                         if (data.success) {
                         } else {
                             if (data.userzt == 2) {
+                                alert(11111);
                                 Toast({
                                     message: '此用户已被删除或被禁用，请联系管理员！',
                                     position: 'bottom'
