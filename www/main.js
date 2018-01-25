9f5747bfbea5022ba1d6748d16662c952c417488
wangqian188 <m18811383660_1@163.com>
2018-01-25 17:40:39 +0800

    销售-合并2

--- www/main.js
+++ www/main.js
@@ -16,8 +16,8 @@ import { Search } from 'mint-ui';
 Vue.component(Search.name, Search);
 
 
-// Vue.prototype.$prefix = "http://47.92.145.21:81" //图片前缀
-Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
+Vue.prototype.$prefix = "http://47.92.145.21:81" //图片前缀
+// Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
 
 
 
@@ -27,9 +27,7 @@ Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
 // 生产环境
 Vue.prototype.$api = "http://omc.urskongjian.com" //api地址
 //Vue.prototype.$api = "http://yhcms.tunnel.qydev.com" //api地址本地
-
-
-Vue.prototype.$api = "http://116.62.68.26:8080" //api地址116的地址
+//Vue.prototype.$api = "http://116.62.68.26:8080" //api地址116的地址
 
 Vue.config.debug = true;// 开启debug模式
 
@@ -461,7 +459,7 @@ router.beforeEach(function(to, from, next){
                 }else{
                     next({path: '/login'});
                 }
-                $.post("http://116.62.68.26:8080/yhcms/web/wxqx/getXsLogin.do", {
+                $.post("http://omc.urskongjian.com/yhcms/web/wxqx/getXsLogin.do", {
                         "foreEndType": 2,
                         "code": "300000045",
                         "cookie": user22.sjs,
