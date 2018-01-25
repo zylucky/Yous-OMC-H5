d2d630f9347fa35545875a693d34c3efd1f3affa
yuan <zylucying@163.com>
2018-01-25 14:05:47 +0800

    更新2018-01-25 14:05 pm

--- www/main.js
+++ www/main.js
@@ -17,18 +17,18 @@ import { Search } from 'mint-ui';
 Vue.component(Search.name, Search);
 
 
-// Vue.prototype.$prefix = "http://47.92.145.21:81" //图片前缀
-Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
+   Vue.prototype.$prefix = "http://47.92.145.21:81" //图片前缀
+//Vue.prototype.$prefix = "http://116.62.68.26:81" //图片前缀
 
 
 // 测试环境
 //Vue.prototype.$api = "http://192.168.0.222:8080" //api地址
 
 // 生产环境
-//Vue.prototype.$api = "http://omc.urskongjian.com" //api地址
+Vue.prototype.$api = "http://omc.urskongjian.com" //api地址
 //Vue.prototype.$api = "http://yhcms.tunnel.qydev.com" //api地址本地
 
-Vue.prototype.$api = "http://116.62.68.26:8080" //api地址116的地址
+//Vue.prototype.$api = "http://116.62.68.26:8080" //api地址116的地址
 // Vue.prototype.$api = "http://192.168.21.55:8080" //
 // Vue.prototype.$api = "http://192.168.23.144:8080" //api地址116的地址
 Vue.config.debug = true;// 开启debug模式
@@ -461,7 +461,7 @@ router.beforeEach(function(to, from, next){
                 }else{
                     next({path: '/login'});
                 }
-                $.post("http://116.62.68.26:8080/yhcms/web/wxqx/getXsLogin.do", {
+                $.post("http://omc.urskongjian.com/yhcms/web/wxqx/getXsLogin.do", {
                         "foreEndType": 2,
                         "code": "300000045",
                         "cookie": user22.sjs,
